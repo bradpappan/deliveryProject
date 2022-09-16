b88b05bb7f76f791802d911c384d044aa2501f7c
Brad Pappan <brad.pappan2@gmail.com>
2022-09-15 19:14:59 -0500

    added hash table

--- Hash.py
+++ Hash.py
@@ -1,4 +1,47 @@
 # Hash map
+'''
+import pandas as pd
+df = pd.read_csv(r'C:\Users\nelli\Desktop\WGUPS Package File.csv')
+'''
+
+import csv
 
 class HashMap:
-    def __init__(self):
+    def __init__(self, initial_capacity = 10):
+        self.table = []
+        for i in range(initial_capacity):
+            self.table.append([])
+
+    def insert(self, key, item):
+        bucket = hash(key) % len(self.table)
+        bucket_list = self.table[bucket]
+
+        for kv in bucket_list:
+            if kv[0] == key:
+                kv[1] = item
+                return True
+
+        key_value = [key, item]
+        bucket_list.append(key_value)
+        return True
+
+    def search (self, key):
+        bucket = hash(key) % len(self.table)
+        bucket_list = self.table[bucket]
+
+        for kv in bucket_list:
+            if kv[0] == key:
+                return kv[1]
+            return None
+
+    def remove(self, key):
+        bucket = hash(key) % len(self.table)
+        bucket_list = self.table[bucket]
+
+        for kv in bucket_list:
+            if kv[0] == key:
+                bucket_list.remove([kv[0], kv[1]])
+
+class Package:
+    def __init__(self)
+
