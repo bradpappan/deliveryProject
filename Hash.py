2483ce91e6da43aad490816798cb261673c39470
bradpappan <brad.pappan2@gmail.com>
2022-09-16 14:59:55 -0500

    package file added and edited

--- Hash.py
+++ Hash.py
@@ -7,7 +7,7 @@ df = pd.read_csv(r'C:\Users\nelli\Desktop\WGUPS Package File.csv')
 import csv
 
 class HashMap:
-    def __init__(self, initial_capacity = 10):
+    def __init__(self, initial_capacity = 40):
         self.table = []
         for i in range(initial_capacity):
             self.table.append([])
@@ -42,6 +42,47 @@ class HashMap:
             if kv[0] == key:
                 bucket_list.remove([kv[0], kv[1]])
 
+'''add in status, time it left the hub, and time delivered '''
+
 class Package:
-    def __init__(self)
+    def __init__(self, packageid, address, city, state, zipcode, deadline, mass, notes):
+        self.packageid = packageid
+        self.address = address
+        self.city = city
+        self.state = state
+        self.zipcode = zipcode
+        self.deadline = deadline
+        self.mass = mass
+        self.notes = notes
+
+    def __str__(self):
+        return "%s, %s, %s, %s, %s, %s, %s, %s," % (self.packageid, self.address, self.city, self.state, self.zipcode, self.deadline, self.mass, self.notes)
+
+
+def loadpackagedata(fileName):
+    with open(fileName) as packages:
+        packageData = csv.reader(packages, delimiter=',')
+        next(packageData)
+        for package in packageData:
+            pId = int(package[0])
+            pAddress = package[1]
+            pCity = package[2]
+            pState = package[3]
+            pZipcode = package[4]
+            pDeadline = package[5]
+            pMass = package[6]
+            pNotes = package[7]
+
+            p = Package(pId, pAddress, pCity, pState, pZipcode, pDeadline, pMass, pNotes)
+
+            myHash.insert(pId, p)
+
+
+myHash = HashMap()
+
+loadpackagedata('packageFile.csv')
+
+for i in range(len(myHash.table)+1):
+    print("Package: {}" .format(myHash.search(i+1)))
+
 
