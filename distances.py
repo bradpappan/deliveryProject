0deefe9ed8347d30148d6293779a0b559d32fd9a
Brad Pappan <brad.pappan2@gmail.com>
2022-11-15 17:33:52 -0600

    greedy algorithm added

--- distances.py
+++ distances.py
@@ -1,4 +1,5 @@
 import csv
+import sys
 
 
 def load_distance_data(dis_data):
@@ -40,11 +41,95 @@ address_data = []
 load_address_data(address_data)
 print(address_data)
 
-with open('csvFiles/distanceData.csv') as distanceNumbers:
-    distanceCsv = list(csv.reader(distanceNumbers, delimiter=','))
+with open('csvFiles/distanceData.csv') as distance_numbers:
+    distance_csv = list(csv.reader(distance_numbers, delimiter=','))
 
 with open('csvFiles/addressData.csv') as addresses:
-    addressCsv = list(csv.reader(addresses, delimiter=','))
+    address_csv = list(csv.reader(addresses, delimiter=','))
 
     def get_distance(row, col, total):
-        distance
+        distance = distance_csv[row][col]
+        if distance == '':
+            distance = distance_csv[row][col]
+        return total + float(distance)
+
+    def current_distance(row, col):
+        distance = distance_csv[row][col]
+        if distance == '':
+            distance = distance_csv[row][col]
+        return float(distance)
+
+
+first_truck = []
+first_truck_index_list = []
+second_truck = []
+second_truck_index_list = []
+third_truck = []
+third_truck_index_list = []
+
+
+def shortest_path(truck_list, truck, current_location):
+    if len(list) == 0:
+        return list
+    else:
+        try:
+            lowest_number = 100.0
+            new_location = 0.0
+            for index in truck_list:
+                if current_distance(current_location, int(index[1])) <= lowest_number:
+                    lowest_number = current_distance(current_location, int(index[1]))
+                    new_location = int(index[1])
+            for index in truck_list:
+                if current_distance(current_location, int(index[1])) == lowest_number:
+                    if truck == 1:
+                        first_truck.append(index)
+                        first_truck_index_list.append(index[1])
+                        pop_value = truck_list.index(index)
+                        truck_list.pop(pop_value)
+                        current_location = new_location
+                        shortest_path(truck_list, 1, current_location)
+                    elif truck == 2:
+                        second_truck.append(index)
+                        second_truck_index_list.append(index[1])
+                        pop_value = truck_list.index(index)
+                        truck_list.pop(pop_value)
+                        current_location = new_location
+                        shortest_path(truck_list, 2, current_location)
+                    elif truck == 3:
+                        third_truck.append(index)
+                        second_truck_index_list.append(index[1])
+                        pop_value = truck_list.index(index)
+                        truck_list.pop(pop_value)
+                        current_location = new_location
+                        shortest_path(truck_list, 3, current_location)
+        except IndexError:
+            pass
+
+
+first_truck_index_list.insert(0, '0')
+second_truck_index_list.insert(0, '0')
+third_truck_index_list.insert(0, '0')
+
+
+def first_truck_index():
+    return first_truck_index_list
+
+
+def first_truck_list():
+    return first_truck
+
+
+def second_truck_index():
+    return second_truck_index_list
+
+
+def second_truck_list():
+    return second_truck
+
+
+def third_truck_index():
+    return third_truck_index_list
+
+
+def third_truck_list():
+    return third_truck
