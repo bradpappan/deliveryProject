e929d869560a53ea4adc500c0b686f6f6310312f
Brad Pappan <brad.pappan2@gmail.com>
2022-11-19 00:05:09 -0600

    added truck class and finished distance class

--- main.py
+++ main.py
@@ -1,5 +1,21 @@
-# Brad Pappan
-# Student ID - 002656581
+# Brad Pappan, Student ID - 002656581
+import distances
+import packages
+
 
 class Main:
-    print("")
+    print("Parcel Service")
+    print("Total miles for all three truck delivery routes is: " + packages.first_truck.mileage +
+          packages.second_truck.mileage + packages.third_truck.mileage)
+
+    program_start = input("Type 'begin' to start the program, or type 'exit' to quit.")
+
+
+    if program_start == "begin":
+        try:
+            print("\n Please select one of the options to search package by ID")
+            print("1) Look up a single package\n")
+            print("2) Check the status of all packages\n")
+
+            selection = input("Enter the the number for the choice: ")
+
