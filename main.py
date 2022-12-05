81ac1b8f3a2981c69e3853874ded38e0fa6944a1
Brad Pappan <brad.pappan2@gmail.com>
2022-12-05 17:24:09 -0600

    total miles works as intended

--- main.py
+++ main.py
@@ -116,15 +116,14 @@ def total_miles():
 #    print("Package: {}" .format(my_hash.search(i)))
 
 print("Parcel Service")
-print("Total miles for all three truck delivery routes is: ")
-print(total_miles())
+print("Total miles for all three truck delivery routes is:", total_miles())
 
 program_start = input("Type 'begin' to start the program and display where the packages are by time, or type "
-                      "'exit' to quit.")
+                      "'exit' to quit. ")
 
 if program_start == "begin":
     try:
-        location_lookup = input("Enter a time to check where a package is using HH:MM:SS format.")
+        location_lookup = input("Enter a time to check where a package is using HH:MM:SS format. ")
         (hh, mm, ss) = location_lookup.split(":")
         time_conversion = datetime.timedelta(hours=int(hh), minutes=int(mm), seconds=int(ss))
 
